a77b3954cd6783ce5a123d7bcf955293a2b0d31e
rhit-martincw <152203014+rhit-martincw@users.noreply.github.com>
2025-02-10 12:21:54 -0500

    Add files via upload
    SPEAKERS!!!!!

---
+++ speaker.c
@@ -0,0 +1,8 @@
+/*
+ * speaker.c
+ *
+ *  Created on: Feb 10, 2025
+ *      Author: martincw
+ */
+
+
---
+++ speaker.h
@@ -0,0 +1,13 @@
+/*
+ * speaker.h
+ *
+ *  Created on: Feb 10, 2025
+ *      Author: martincw
+ */
+
+#ifndef SPEAKER_H_
+#define SPEAKER_H_
+
+
+
+#endif /* SPEAKER_H_ */
