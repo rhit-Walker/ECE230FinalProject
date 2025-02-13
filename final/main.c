7f388eb5f91dbeab6e619fdf01bf30ebf8252d13
walkerqr <walkerqr@rose-hulman.edu>
2025-02-13 11:25:13 -0500

    w

--- final/main.c
+++ final/main.c
@@ -10,4 +10,8 @@
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	initTimer();
+	initButtons();
+	initLEDS();
+	initLCD8Bit();
 }
---
+++ final/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: Feb 13, 2025
+ *      Author: walkerqr
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+
+#endif /* MAIN_H_ */
