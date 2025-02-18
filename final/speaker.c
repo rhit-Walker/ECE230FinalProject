e6edce4c497aeb4892d2ce350face68147356f6f
rhit-martincw <152203014+rhit-martincw@users.noreply.github.com>
2025-02-18 12:41:31 -0500

    Speakers should work with the 'Carnival' music
    Remade with TA0 and TA2, this means great changes are coming

--- final/speaker.c
+++ final/speaker.c
@@ -5,4 +5,82 @@
  *      Author: martincw
  */
 
+#include <msp.h>
 
+#include <stdio.h>
+#include "csHFXT.h"
+#include "csLFXT.h"
+#include "speaker.h"
+
+void PlayerConfiguration(void){
+    SpeakerPort->DIR |= Speaker;            // set P2.4 as output
+    SpeakerPort->SEL0 |= Speaker;           // P2.4 set to TA0.1
+    SpeakerPort->SEL1 &= ~Speaker;          // 0b01 (secondary mode)
+    TIMER_A0->CCR[0] = NOTEA4 - 1;          // TA0 CCR0 compare for period
+    TIMER_A0->CCR[1] = (NOTEA4 / 2) - 1;    // TA0 CCR1 compare for duty cycle of the note
+    TIMER_A0->CCTL[1] = 0b0000000001100000;
+    TIMER_A0->CTL = 0b0000001010010100;
+}
+
+void PlayNote(unsigned int CurrentNote){   // Set high pulse-width in CCR1 register
+    TIMER_A0->CCR[0] = CurrentNote - 1;
+    TIMER_A0->CCR[1] = (CurrentNote / 2) - 1;
+}
+
+void NoteDurationConfiguration(void){      // Configure note duration and interrupts
+    TIMER_A2->CCR[0] = HALF_NOTE;
+    TIMER_A2->CCTL[0]=0x0010;
+    TIMER_A2->CTL=0b0000000100010100;
+    NVIC->ISER[0] |= (1)<<TA2_0_IRQn;
+}
+
+void SignalConfigured(const uint16_t notelist[]){ // Signal that configuration is done
+    static char noteindex;
+    unsigned int delay;
+    noteindex=0;
+    while(notelist[noteindex]!=NULL) {
+        PlayNote(notelist[noteindex]);
+        noteindex=noteindex+1;
+        for(delay = NoteDuration; delay != 0; delay--) {
+         }
+    }
+}
+
+void TA2_0_IRQHandler(void){
+    static char nextnote=0;     //start with first note in a song
+    static char insert_rest=1;
+
+    if(TIMER_A2->CCTL[0] & TIMER_A_CCTLN_CCIFG) // CCR0 interrupt
+     {
+         TIMER_A2->R = 0;                       // Clear Timer A1
+         if (insert_rest==1) {                  // Insert a rest (speaker = off)
+             insert_rest=0;
+             TIMER_A2->CCR[0] = DELAY100MS;
+             SpeakerPort->DIR &= ~Speaker;
+         } else {                               // Otherwise play the next Note
+             insert_rest=1;                     // 100 ms rest
+             SpeakerPort->DIR |= Speaker;
+             if(CarnivalNotes[nextnote] != NULL){
+                 nextnote=nextnote+1;           // increment to next Note
+             } else {
+                 nextnote = 0;                  // NULL, repeat the piece
+             }
+             PlayNote(CarnivalNotes[nextnote]); // Play the Note
+             switch(CarnivalBeats[nextnote]){   // Play at the specified duration
+                 case 1: TIMER_A2->CCR[0] = FULL_NOTE;
+                     break;
+                 case 2: TIMER_A2->CCR[0] = HALF_NOTE;
+                     break;
+                 case 4: TIMER_A2->CCR[0] = QUARTER_NOTE;
+                     break;
+                 case 8: TIMER_A2->CCR[0] = EIGHTH_NOTE;
+                     break;
+                 case 16: TIMER_A2->CCR[0] = SIXTEENTH_NOTE;
+                     break;
+                 default: TIMER_A2->CCR[0] = QUARTER_NOTE;
+                     break;
+             }
+         }
+         TIMER_A2->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG; // Clear flag
+     }
+}
--- final/speaker.h
+++ final/speaker.h
@@ -8,6 +8,135 @@
 #ifndef SPEAKER_H_
 #define SPEAKER_H_
 
+#define SpeakerPort P2
+#define Speaker BIT4     // P2.4
+#define LED1port P1
+#define LED1 BIT0
 
+typedef enum  {STOP, START, WAIT, PAUSE} PlayerStatus;
+typedef enum _SwitchState {Pressed, NotPressed} SwitchState;
+
+#define SMCLK 48000000     //Hz
+#define TimerA0Prescaler 4 //Timer A prescaler
+#define TimerA0Clock  SMCLK/TimerA0Prescaler
+
+#define RestNote 1
+
+#define FrequencyC4 261.626
+#define FrequencyE4 329.628
+#define FrequencyF4 349.228
+#define FrequencyFsGb4 369.994
+#define FrequencyGsAb4 415.305
+#define FrequencyA4 440
+#define FrequencyB4 493.88
+#define FrequencyCsDb5 554.365
+#define FrequencyDsEb5 622.254
+#define FrequencyE5 659.255
+#define FrequencyF5 698.456
+#define FrequencyFsGb5 739.989
+#define FrequencyG5 783.991
+#define FrequencyGsAb5 830.609
+#define FrequencyB5 987.767
+
+#define NOTEC4  TimerA0Clock/FrequencyC4
+#define NOTEE4  TimerA0Clock/FrequencyE4
+#define NOTEF4  TimerA0Clock/FrequencyF4
+#define NOTEFsGb4  TimerA0Clock/FrequencyFsGb4
+#define NOTEGsAb4  TimerA0Clock/FrequencyGsAb4
+#define NOTEA4  TimerA0Clock/FrequencyA4
+#define NOTEB4  TimerA0Clock/FrequencyB4
+#define NOTECsDb5  TimerA0Clock/FrequencyCsDb5
+#define NOTEDsEb5  TimerA0Clock/FrequencyDsEb5
+#define NOTEE5  TimerA0Clock/FrequencyE5
+#define NOTEF5  TimerA0Clock/FrequencyF5
+#define NOTEFsGb5  TimerA0Clock/FrequencyFsGb5
+#define NOTEG5  TimerA0Clock/FrequencyG5
+#define NOTEGsAb5  TimerA0Clock/FrequencyGsAb5
+#define NOTEB5  TimerA0Clock/FrequencyB5
+
+const uint16_t NotesSequence[] = {NOTEE5, NOTEDsEb5, NOTEE5, NOTEFsGb5, NOTEGsAb5, NOTEE5, RestNote, NULL};
+
+const uint16_t CarnivalNotes[] = {RestNote, NOTEE5, NOTEDsEb5, NOTEFsGb4, RestNote, NOTEE5, NOTEFsGb5, NOTEFsGb5, NOTEFsGb4, NOTEFsGb4, RestNote,
+                             NOTEGsAb4, NOTEGsAb4, RestNote, NOTECsDb5, NOTECsDb5, RestNote, //Star
+                             NOTEFsGb4, NOTEB4, RestNote, NOTEB4, RestNote, NOTEGsAb4, NOTEB4, NOTEB4, RestNote, NOTEFsGb4, NOTEB4, NOTEA4, NOTEGsAb4, NOTEE4,
+                             RestNote, //Repeat1
+                             NOTEFsGb4, NOTEB4, RestNote, NOTEB4, RestNote, NOTEGsAb4, NOTEB4, NOTEB4, RestNote, NOTEFsGb4, NOTEB4, NOTEA4, NOTEGsAb4, NOTEE4,
+                             NOTEE5, NOTEDsEb5, NOTEFsGb4, RestNote, NOTEE5, NOTEFsGb5, NOTEFsGb5, NOTEFsGb4, NOTEFsGb4, RestNote,
+                             NOTEGsAb4, NOTEGsAb4, RestNote, NOTECsDb5, NOTECsDb5, // beginning repeats
+                             RestNote, NOTEFsGb4, NOTEB4, RestNote, NOTEB4, RestNote, NOTEE5, NOTEFsGb5, NOTEB5, NOTEFsGb5, NOTEF5, //Star
+                             NOTEE5, NOTEDsEb5, NOTEB4, NOTEB4, NOTEFsGb4, NOTEB4, RestNote, NOTEFsGb4, RestNote, NOTEFsGb4,
+                             NOTEB4, NOTEB4, NOTEF4, RestNote, NOTEE5, NOTEFsGb5, NOTEB5, NOTEFsGb5, NOTEF5,
+                             NOTEB4, NOTEB4, NOTEFsGb4, NOTEB4, RestNote, NOTEFsGb4, RestNote, NOTEFsGb4,
+                             NOTEB4, NOTEB4, NOTEF4, RestNote, NOTEE5, NOTEFsGb5, NOTEB5, NOTEFsGb5, NOTEF5,
+                             NOTEB4, NOTEB4, NOTEFsGb4, NOTEB4, RestNote, NOTEFsGb4, RestNote, NOTEFsGb4,
+                             NOTEB4, NOTEB4, NOTEF4, RestNote, NOTEE5, NOTEFsGb5, NOTEB5, NOTEFsGb5, NOTEF5, NULL};
+const uint16_t CarnivalBeats[] = {8, 2, 2, 8, 8, 8, 8, 8, 8, 8,
+                                  8, 4, 8, 8, 4, 8, 8, 8, 8, 8,
+                                  4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, //repeat
+                                  8, 8, 8, 8, 4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, //end repeat
+                                  2, 2, 4, 8, 8, 8, 8, 8, 8, 8, 4, 8, 8, 4, 8, // beginning repeats
+                                  8, 8, 8, 8, 4, 8, 16, 16, 8, 8, 8,
+                                  2, 2, 8, 8, 8, 8, 8, 8, 8, 8,
+                                  8, 8, 8, 8, 16, 16, 8, 8, 8,
+                                  8, 8, 8, 8, 8, 8, 8, 8, // Repeat 1
+                                  8, 8, 8, 8, 16, 16, 8, 8, 8,
+                                  8, 8, 8, 8, 8, 8, 8, 8, // Repeat 2
+                                  8, 8, 8, 8, 16, 16, 8, 8, 8, NULL};
+
+#define ACLK 32768 //Hz
+#define FULL_NOTE       ACLK    //1 second
+#define HALF_NOTE   ACLK/2  //0.5 second
+#define QUARTER_NOTE    ACLK/4  //0.25 second
+#define EIGHTH_NOTE  ACLK/8
+#define SIXTEENTH_NOTE ACLK/16
+#define DELAY100MS  ACLK/10
+
+#define NoteDuration 1000000
+
+
+/*
+ * Configure speaker as the Timer A0 and CCR1 output pin, configured to produce
+ * sound on CCR1 output compare.
+ *
+ * \return None
+ */
+extern void PlayerConfiguraion(void);
+
+
+/*
+ * Play a note, send the Timer A0 cycle count to play the next note within an
+ * array of Notes
+ *
+ * \return None
+ */
+extern void PlayNote(unsigned int CurrentNote);
+
+
+/*
+ * Configure Timer A1 and CCR0 to generate the note duration.
+ * Configure the interrupts for the Speaker.
+ *
+ * \return None
+ */
+extern void NoteDurationConfiguration(void);
+
+
+/*
+ * Play a small sequence of Notes to signal that configuration has finished,
+ * and that the system is now awaiting user input to start.
+ *
+ *
+ * \return None
+ */
+extern void SignalConfigured(const uint16_t notelist[]);
+
+/*
+ * Interrupt handler used to play the next note within the Note with a specific beat
+ * from the Note and Beat arrays. Generates a 100ms rest between notes similar to
+ * project 3, as the piece we are using is intended to be played 'staccato'.
+ *
+ * \return None
+ */
+void TA2_0_IRQHandler(void);
 
 #endif /* SPEAKER_H_ */
